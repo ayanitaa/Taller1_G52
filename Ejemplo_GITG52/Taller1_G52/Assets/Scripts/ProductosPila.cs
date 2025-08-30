639bbef71eaa6caed29877f202ab7420ad21b81d
Juan0049 <juanpab.0409@gmail.com>
2025-08-30 09:50:17 -0500

    Scripts organizados dentro de la carpeta

--- Ejemplo_GITG52/Taller1_G52/Assets/Scripts/ProductosPila.cs
+++ Ejemplo_GITG52/Taller1_G52/Assets/Scripts/ProductosPila.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ProductoPila : MonoBehaviour
+public class ProductosPila : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
