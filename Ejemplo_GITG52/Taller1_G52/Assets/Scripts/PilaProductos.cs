8eef0af96dc642d9fc9e1df8aee8452a770dff20
ayanitaa <ayanasalazar7@gmail.com>
2025-09-01 13:00:25 -0500

    Lógica Pilas

---
+++ Ejemplo_GITG52/Taller1_G52/Assets/Scripts/PilaProductos.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections.Generic;
+using packageProductosPila;
+
+public class PilaProductos : MonoBehaviour
+{
+    private Stack<Producto> pila;
+
+    public void Apilar (Producto producto)
+    {
+        pila.Push(producto);
+    }
+
+    public Producto Desapilar()
+    {
+        if (pila.Count > 0)
+        {
+            return pila.Pop();
+        }
+        else
+        {
+            Debug.LogWarning("La pila está vacía. No se puede desapilar.");
+            return null;
+        }
+    }
+
+    public int tamañoPila()
+    {
+        return pila.Count;
+    }
+    public Producto VerTope()
+    {
+        if (pila.Count > 0)
+        {
+            return pila.Peek();
+        }
+        else
+        {
+            Debug.LogWarning("La pila está vacía. No hay tope.");
+            return null;
+        }
+    }
+}
