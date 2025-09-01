0c2f1bcbf62d30cb9958ca3a9d30ebfd1e9803ee
mariajo228 <maria_jose.asprilla@uao.edu.co>
2025-09-01 15:56:26 -0500

    JSON y interfaz

--- Ejemplo_GITG52/Taller1_G52/Assets/Scripts/Export JSON.cs
+++ Ejemplo_GITG52/Taller1_G52/Assets/Scripts/Export JSON.cs
@@ -6,4 +6,26 @@ using System.IO;
 
 public class ExportJSON : MonoBehaviour
 {
+    public float total_generado;
+    public float total_despachados;
+    public float total_pila;
+    public int tiempo_promedio_despacho;
+    public int despachados_por_tipo;
+    public Dictionary<string, int> despachadosPorTipo;
+    public string tipoMasDespachado;
+
 }
+
+public class Exportador
+{
+    public static void Guardar(ExportJSON j)
+    {
+        string json = JsonUtility.ToJson(r, true);
+
+        string ruta = Path.Combine(Application.persistentDataPath, "resultados.json");
+        File.WriteAllText(ruta, json);
+
+        Debug.Log("Resultados guardados en: " + ruta);
+        Debug.Log(json);
+       }
+    }
