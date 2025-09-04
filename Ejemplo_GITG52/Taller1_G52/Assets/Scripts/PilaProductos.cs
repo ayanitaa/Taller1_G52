27a9f7fab8c783dcc37fd39922e107ffc1e917d7
ayanitaa <ayanasalazar7@gmail.com>
2025-09-04 13:24:25 -0500

    Cambio caracteres latinos

--- Ejemplo_GITG52/Taller1_G52/Assets/Scripts/PilaProductos.cs
+++ Ejemplo_GITG52/Taller1_G52/Assets/Scripts/PilaProductos.cs
@@ -25,7 +25,7 @@ public class PilaProductos : MonoBehaviour
         }
         else
         {
-            Debug.LogWarning("La pila está vacía. No se puede desapilar.");
+            Debug.LogWarning("La pila esta vacia. No se puede desapilar.");
             return null;
         }
     }
@@ -43,7 +43,7 @@ public class PilaProductos : MonoBehaviour
         }
         else
         {
-            Debug.LogWarning("La pila está vacía. No hay tope.");
+            Debug.LogWarning("La pila esta vacia. No hay tope.");
             return null;
         }
     }
